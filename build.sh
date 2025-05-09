9504f52ccd6983b0e23cf710c383f45ca8678bae
Ujwal Singh <160722607+coderujwal3@users.noreply.github.com>
2025-05-09 14:00:22 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Title Banner 
-echo -e "\n==============================" | lolcat
-echo "   🚀 Coder Ujwal's Java Tool" | lolcat
-echo -e "==============================\n" | lolcat
+echo -e "\n==============================" | lolcat -a
+echo "   🚀 Coder Ujwal's Java Tool" | lolcat -a
+echo -e "==============================\n" | lolcat -a
 
 # Compiling the Encode and Decode files
-echo "[*] Compiling Encode.java..." | lolcat
-javac --release 17 Encode.java && echo "[✔] Encode.java compiled!" | lolcat
+echo "[*] Compiling Encode.java..." | lolcat -a
+javac --release 17 Encode.java && echo "[✔] Encode.java compiled!" | lolcat -a
 
-echo "[*] Compiling Decode.java..." | lolcat
-javac --release 17 Decode.java && echo "[✔] Decode.java compiled!" | lolcat
+echo "[*] Compiling Decode.java..." | lolcat -a
+javac --release 17 Decode.java && echo "[✔] Decode.java compiled!" | lolcat -a
 
 # Last Message 
-echo -e "\nReady to use!" | lolcat
-echo "[::] Run 'java Encode' to encode a message." | lolcat
-echo "[::] Run 'java Decode' to decode a message." | lolcat
-echo -e "\nHappy Coding!" | lolcat
+echo -e "\nReady to use!" | lolcat -a
+echo "[::] Run 'java Encode' to encode a message." | lolcat -a
+echo "[::] Run 'java Decode' to decode a message." | lolcat -a
+echo -e "\nHappy Coding!" | lolcat -a
